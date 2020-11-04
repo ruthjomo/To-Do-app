7f76a9ae4206d89962fb06516078eceaaa703f6b
ruthjomo <ruthjomo19@gmail.com>
2020-11-04 12:35:10 +0300

    added installation

--- theapp/models.py
+++ theapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='images/', blank=True)
+    contact = HTMLField()
+    email = models.EmailField(max_length=70, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    '''
+    this is added to ensure the linter has no errors saying class has no objects member in VS Code IDE
+    '''
+    objects = models.Manager()
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls, id):
+        profile = Profile.objects.get(user=id)
+        return profile
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def find_profile(cls, search_term):
+        profile = Profile.objects.filter(user__username__icontains=search_term)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user=id).first()
+        return profile
+
+    class Meta:
+        ordering = ['user']
+
