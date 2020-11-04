c687a37d9806930c800c9cb7738328e6b9dd8bb4
ruthjomo <ruthjomo19@gmail.com>
2020-11-04 12:44:24 +0300

    added codes

--- theapp/views.py
+++ theapp/views.py
@@ -23,6 +23,19 @@ def home(request):
         hoods = Neighborhood.all_neighborhoods()
         return render(request, 'index.html', {"hoods": hoods})
 
+@login_required(login_url='/accounts/login/')
+def user_profile(request, username):
+    profile = User.objects.get(username=username)
+    try:
+        profile_info = Profile.get_profile(profile.id)
+    except:
+        profile_info = Profile.filter_by_id(profile.id)
+    businesses = Business.get_profile_businesses(profile.id)
+    title = f'@{profile.username}'
+    return render(request, 'profile.html', {'title': title, 'profile': profile, 'profile_info': profile_info, 'businesses': businesses})
+
+
+
 
 @login_required(login_url='/accounts/login/')
 def add_profile(request):
