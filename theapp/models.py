f8866c3f4fabf788e5479871b8f3296dbaf40f53
ruthjomo <ruthjomo19@gmail.com>
2020-11-04 14:49:52 +0300

    added changes

--- theapp/models.py
+++ theapp/models.py
@@ -6,6 +6,11 @@ from django.db import models
 # # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    complete= models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 #     contact = HTMLField()
 #     email = models.EmailField(max_length=70, blank=True)
 #     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
