668b609af59687152f6bcf887be8603ca5ff72f6
ruthjomo <ruthjomo19@gmail.com>
2020-11-04 12:38:04 +0300

    added files

--- theapp/views.py
+++ theapp/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import NewProfileForm
+import datetime as dt
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        if Join.objects.filter(user_id=request.user).exists():
+            hood = Neighborhood.objects.get(pk=request.user.join.hood_id.id)
+            posts = Post.objects.filter(post_hood=request.user.join.hood_id.id)
+            businesses = Business.objects.filter(
+                business_hood=request.user.join.hood_id.id)
+            return render(request, 'current_hood.html', {"hood": hood, "businesses": businesses, "posts": posts})
+        else:
+            hoods = Neighborhood.all_neighborhoods()
+            return render(request, 'index.html', {"hoods": hoods})
+    else:
+        hoods = Neighborhood.all_neighborhoods()
+        return render(request, 'index.html', {"hoods": hoods})
+
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'new_profile.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
